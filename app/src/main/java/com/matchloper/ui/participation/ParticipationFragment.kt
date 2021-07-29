fa1a47d80d923021f4302e3ad2a08d262d49089b
garam918 <62510764+garam918@users.noreply.github.com>
2021-07-29 16:06:18 +0900

    Feat : 매칭 참여 Fragment에 DataBinding 정의

--- app/src/main/java/com/matchloper/ui/participation/ParticipationFragment.kt
+++ app/src/main/java/com/matchloper/ui/participation/ParticipationFragment.kt
@@ -4,25 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.matchloper.R
+import com.matchloper.databinding.FragmentParticipationBinding
 
 class ParticipationFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var binding: FragmentParticipationBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_participation, container, false)
+    ): View {
 
-        return root
+        homeViewModel = ViewModelProvider(this.requireActivity()).get(HomeViewModel::class.java)
+
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_participation, container, false)
+        binding.lifecycleOwner = this
+        binding.participationViewModel = homeViewModel
+
+        return binding.root
     }
 }
