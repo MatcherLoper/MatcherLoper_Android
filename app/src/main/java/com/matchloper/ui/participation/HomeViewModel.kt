ecebe83eb068f8295946cc3e60c6cb74096a2b8b
garam918 <62510764+garam918@users.noreply.github.com>
2021-07-29 16:04:34 +0900

    Feat : 메인 ViewModel 정의

--- app/src/main/java/com/matchloper/ui/participation/HomeViewModel.kt
+++ app/src/main/java/com/matchloper/ui/participation/HomeViewModel.kt
@@ -1,13 +1,28 @@
 package com.matchloper.ui.participation
 
+import androidx.databinding.ObservableArrayList
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.matchloper.data.PositionInfoData
 
 class HomeViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    val id = MutableLiveData<Int>().apply {
+        this.value = 1
     }
-    val text: LiveData<String> = _text
+
+    val teamProjectName = MutableLiveData<String>()
+    val positionData = ObservableArrayList<PositionInfoData>()
+    val position = MutableLiveData<String>()
+    val count = MutableLiveData<String>()
+
+    fun addPositionInfo() {
+        if(position.value != "" && count.value != "" && count.value!!.toInt() > 0) {
+            positionData.add(PositionInfoData(position.value.toString(), count.value!!.toInt()))
+            position.value = ""
+            count.value = ""
+        }
+    }
+
 }
