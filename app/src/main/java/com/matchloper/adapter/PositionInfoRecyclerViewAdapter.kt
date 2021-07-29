31424ca5069f33abe9d7285a567360d66e4c527a
garam918 <62510764+garam918@users.noreply.github.com>
2021-07-29 16:08:15 +0900

    Feat : 팀원 포지션을 정하는 RecyclerView Adapter 정의

---
+++ app/src/main/java/com/matchloper/adapter/PositionInfoRecyclerViewAdapter.kt
@@ -0,0 +1,33 @@
+package com.matchloper.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.ObservableArrayList
+import androidx.recyclerview.widget.RecyclerView
+import com.matchloper.data.PositionInfoData
+import com.matchloper.databinding.PositionInfoLayoutBinding
+
+class PositionInfoRecyclerViewAdapter : RecyclerView.Adapter<PositionInfoRecyclerViewAdapter.ViewHolder>() {
+
+    var items = ObservableArrayList<PositionInfoData> ()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewHolder {
+        val binding = PositionInfoLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: PositionInfoRecyclerViewAdapter.ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    inner class ViewHolder(private val binding : PositionInfoLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: PositionInfoData) {
+            binding.positionInfo = item
+        }
+    }
+}
