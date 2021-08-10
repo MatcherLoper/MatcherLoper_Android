f8c32dfbf1547d14ee944bc90bd31d9171e12d51
garam918 <62510764+garam918@users.noreply.github.com>
2021-08-10 16:31:34 +0900

    Feat : 프로젝트를 열 때 팀원 포지션 목록을 Spinner로 구현

--- app/src/main/java/com/matchloper/view/MatchOpenDialogActivity.kt
+++ app/src/main/java/com/matchloper/view/MatchOpenDialogActivity.kt
@@ -1,6 +1,7 @@
 package com.matchloper.view
 
 import android.os.Bundle
+import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
