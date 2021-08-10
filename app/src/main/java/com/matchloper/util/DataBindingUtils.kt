311a309947610b7e12e1c6062adb743b090d7cbb
garam918 <62510764+garam918@users.noreply.github.com>
2021-08-10 16:36:38 +0900

    Feat : ProjectRecyclerView의 아이템을 BindingAdapter로 데이터 바인딩

--- app/src/main/java/com/matchloper/util/DataBindingUtils.kt
+++ app/src/main/java/com/matchloper/util/DataBindingUtils.kt
@@ -1,13 +1,20 @@
 package com.matchloper.util
 
+import android.app.Activity
 import android.content.Intent
+import android.view.View
+import android.widget.AdapterView
 import android.widget.Button
+import android.widget.Spinner
 import androidx.databinding.BindingAdapter
 import androidx.databinding.ObservableArrayList
+import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.matchloper.adapter.PositionInfoRecyclerViewAdapter
+import com.matchloper.adapter.ProjectRecyclerViewAdapter
 import com.matchloper.data.PositionInfoData
+import com.matchloper.data.ProjectStateData
 import com.matchloper.view.MatchOpenDialogActivity
 
 object DataBindingUtils {
@@ -34,14 +41,24 @@ object DataBindingUtils {
         recyclerView.adapter?.notifyDataSetChanged()
     }
 
-//    @BindingAdapter("spinnerAdapter")
-//    @JvmStatic
-//    fun spinnerAdapter(spinner: Spinner) {
-//        ArrayAdapter.createFromResource(spinner.context, R.array.position_array,
-//        android.R.layout.simple_spinner_item).also {
-//            adapter ->
-//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//            spinner.adapter = adapter
-//        }
-//    }
+    @BindingAdapter("closeDialog")
+    @JvmStatic
+    fun close(button: Button, id: Int) {
+        button.setOnClickListener {
+            (it.context as Activity).finish()
+        }
+    }
+
+    @BindingAdapter("projectList")
+    @JvmStatic
+    fun bindProject(recyclerView: RecyclerView, items: ObservableArrayList<ProjectStateData>) {
+        if(recyclerView.adapter == null) {
+            val lm = LinearLayoutManager(recyclerView.context)
+            val adapter = ProjectRecyclerViewAdapter()
+            recyclerView.layoutManager = lm
+            recyclerView.adapter = adapter
+        }
+        (recyclerView.adapter as ProjectRecyclerViewAdapter).items = items
+        recyclerView.adapter?.notifyDataSetChanged()
+    }
 }
