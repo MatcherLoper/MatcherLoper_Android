33db4212908d23821f660b7156fd0bad035a1743
garam918 <62510764+garam918@users.noreply.github.com>
2021-09-01 20:56:01 +0900

    Feat : 로그인, 회원가입 기능 구현

--- app/src/main/java/com/matchloper/viewModel/MainViewModel.kt
+++ app/src/main/java/com/matchloper/viewModel/MainViewModel.kt
@@ -1,8 +1,10 @@
 package com.matchloper.viewModel
 
+import android.view.View
+import android.widget.AdapterView
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.matchloper.data.SignInRequestData
+import com.matchloper.data.AddressDto
 
 class MainViewModel : ViewModel() {
 
@@ -10,6 +12,23 @@ class MainViewModel : ViewModel() {
     val userPw = MutableLiveData<String>()
     val deviceToken = MutableLiveData<String>()
 
+    val userEmail = MutableLiveData<String>()
+    val userName = MutableLiveData<String>()
+    val userSignUpPw = MutableLiveData<String>()
+    val userPhoneNumber = MutableLiveData<String>()
+    val userAddress = MutableLiveData<String>()
+    val userDetailAddress = MutableLiveData<String>()
+    val userPosition = MutableLiveData<String>()
+    val userSkillInfo = MutableLiveData<String>()
 
+    val positionSpinnerClick = object : AdapterView.OnItemSelectedListener {
+        override fun onNothingSelected(p0: AdapterView<*>?) {
+
+        }
+
+        override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+            if(p0?.getItemAtPosition(p2).toString() != "포지션을 선택하세요") userPosition.value = p0?.getItemAtPosition(p2).toString()
+        }
+    }
 
 }
