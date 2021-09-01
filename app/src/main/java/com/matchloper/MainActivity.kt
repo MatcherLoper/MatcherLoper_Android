16b00ab551e5df5d41cedd0cbd4c12e8efdfe178
garam918 <62510764+garam918@users.noreply.github.com>
2021-09-01 20:58:55 +0900

    Feat : App을 실행했을 때 기기의 토큰을 SharedPreference에 저장

--- app/src/main/java/com/matchloper/MainActivity.kt
+++ app/src/main/java/com/matchloper/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.matchloper
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.matchloper.databinding.ActivityMainBinding
 import com.matchloper.viewModel.MainViewModel
 
@@ -16,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         init()
+        firebaseMessaging()
     }
 
     private fun init() {
@@ -25,5 +29,18 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         binding.viewModel = viewModel
 
+    }
+
+    private fun firebaseMessaging() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if(!task.isSuccessful) {
+                return@OnCompleteListener
+            }
+
+            val token = task.result
+            Log.e("token",token)
+            viewModel.deviceToken.value = token
+
+        })
     }
 }
