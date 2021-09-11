ebd29699e664c2d0bcd3824fa3d78f10d9820863
garam918 <62510764+garam918@users.noreply.github.com>
2021-09-11 21:39:03 +0900

    Feat : 방 상태가 변경되었을 때 Push 메시지 기능 구현

--- app/src/main/java/com/matchloper/FirebaseMessaging.kt
+++ app/src/main/java/com/matchloper/FirebaseMessaging.kt
@@ -1,6 +1,8 @@
 package com.matchloper
 
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -15,6 +17,14 @@ class FirebaseMessaging : FirebaseMessagingService() {
 
         remoteMessage.notification?.let {
             Log.e("Firebase", it.title)
+            Log.e("ff",it.body)
         }
+
+        val notificationBuilder = NotificationCompat.Builder(this, "")
+            .setContentTitle(remoteMessage.notification?.title)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        NotificationManagerCompat.from(this)
+            .notify(1,notificationBuilder.build())
     }
 }
